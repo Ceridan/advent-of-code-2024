a5b035f2c203cd235dc037735996c9ac9d38049d
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2024-12-07 19:38:55 +0100

    refactor: Optimized day 06.

--- src/day06.lua
+++ src/day06.lua
@@ -27,6 +27,81 @@ local function next_dir_idx(idx)
     end
 end
 
+local function compact_map(map)
+    local compacted = {["y"] = {}, ["x"] = {}}
+
+    for y in ipairs(map) do
+        compacted.y[y] = {}
+        for x in ipairs(map[y]) do
+            if map[y][x] == "#" then
+                table.insert(compacted.y[y], x)
+            end
+        end
+    end
+
+    for x in ipairs(map[1]) do
+        compacted.x[x] = {}
+        for y in ipairs(map) do
+            if map[y][x] == "#" then
+                table.insert(compacted.x[x], y)
+            end
+        end
+    end
+
+    return compacted
+end
+
+local function search_compacted(compacted, pos, dir)
+    local map
+    local inner, outer
+    if dir == 1 or dir == 3 then
+        map = compacted.x
+        inner = pos.x
+        outer = pos.y
+    else
+        map = compacted.y
+        inner = pos.y
+        outer = pos.x
+    end
+
+    local l, r = 1, #map[inner]
+    while l <= r do
+        local mid = math.floor((l + r) / 2)
+        if map[inner][mid] > outer then
+            r = mid - 1
+        else
+            l = mid + 1
+        end
+    end
+
+    l, r = r, l
+    if dir == 1 then
+        local y = 0
+        if l > 0 then
+            y = map[inner][l]
+        end
+        return pos.x, y
+    elseif dir == 2 then
+        local x = 0
+        if r <= #map[inner] then
+            x = map[inner][r]
+        end
+        return x, pos.y
+    elseif dir == 3 then
+        local y = 0
+        if r <= #map[inner] then
+            y = map[inner][r]
+        end
+        return pos.x, y
+    elseif dir == 4 then
+        local x = 0
+        if l > 0 then
+            x = map[inner][l]
+        end
+        return x, pos.y
+    end
+end
+
 local function check_visited(map, start)
     local dir_idx = 1
     local curr = start
@@ -40,13 +115,12 @@ local function check_visited(map, start)
             dir_idx = next_dir_idx(dir_idx)
         else
             curr = next_curr
-            local key = curr:key()
-            visited[key] = curr
+            visited[curr:key()] = curr
         end
     end
 end
 
-local function check_cycle(map, start, obstacle)
+local function check_cycle(compacted_map, start, obstacle)
     if start == obstacle then
         return false
     end
@@ -63,13 +137,23 @@ local function check_cycle(map, start, obstacle)
             state[key] = true
         end
 
-        local next_curr = curr + DIRECTIONS[dir_idx]
-        if is_out_of_bound(map, next_curr) then
+        local ox, oy = search_compacted(compacted_map, curr, dir_idx)
+        if ox == 0 or oy == 0 then
             return false
-        elseif next_curr == obstacle or map[next_curr.y][next_curr.x] == "#" then
-            dir_idx = next_dir_idx(dir_idx)
         else
-            curr = next_curr
+            local bidx
+            if dir_idx == 1 then
+                bidx = 3
+            elseif dir_idx == 2 then
+                bidx = 4
+            elseif dir_idx == 3 then
+                bidx = 1
+            else
+                bidx = 2
+            end
+
+            curr = Point2D.new(ox, oy) + DIRECTIONS[bidx]
+            dir_idx = next_dir_idx(dir_idx)
         end
     end
 end
@@ -90,10 +174,35 @@ local function part2(data)
     local map = io.read_text_matrix(data)
     local start = find_start(map)
     local visited = check_visited(map, start)
+    local compacted = compact_map(map)
     local cycle = 0
     for _, vis in pairs(visited) do
-        if check_cycle(map, start, vis) then
-            cycle = cycle + 1
+        if vis ~= start then
+            table.insert(compacted.x[vis.x], vis.y)
+            table.sort(compacted.x[vis.x])
+            table.insert(compacted.y[vis.y], vis.x)
+            table.sort(compacted.y[vis.y])
+
+            if check_cycle(compacted, start, vis) then
+                cycle = cycle + 1
+            end
+
+            local i = 1
+            while true do
+                if compacted.x[vis.x][i] == vis.y then
+                    break
+                end
+                i = i + 1
+            end
+            table.remove(compacted.x[vis.x], i)
+            i = 1
+            while true do
+                if compacted.y[vis.y][i] == vis.x then
+                    break
+                end
+                i = i + 1
+            end
+            table.remove(compacted.y[vis.y], i)
         end
     end
     return cycle
