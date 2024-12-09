0550793487b1320f4187fe14f7fbf1c0d6c59b79
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2024-12-09 20:51:49 +0100

    feat: Day 09, part 2.

--- src/day09.lua
+++ src/day09.lua
@@ -11,6 +11,36 @@ local function parse_input(input)
     return disk
 end
 
+local function build_disk_to_index(disk)
+    local disk_to_index = {}
+    local idx = 0
+    for _, num in ipairs(disk) do
+        table.insert(disk_to_index, idx)
+        idx = idx + num
+    end
+    return disk_to_index
+end
+
+local function build_idx_to_id(disk)
+    local idx_to_id = {}
+    local id = 0
+    for i = 0, #disk-1, 2 do
+        idx_to_id[i+1] = i
+    end
+    return idx_to_id
+end
+
+local function build_free_space_map(disk)
+    local free_space = {}
+    for i = 2, #disk, 2 do
+        if disk[i] > 0 then
+            free_space[disk[i]] = free_space[disk[i]] or {}
+            table.insert(free_space[disk[i]], i)
+        end
+    end
+    return free_space
+end
+
 local function part1(data)
     local disk = parse_input(data)
     local min_id = 0
@@ -51,7 +81,29 @@ local function part1(data)
 end
 
 local function part2(data)
-    return 0
+    local disk = parse_input(data)
+    local disk_to_index = build_disk_to_index(disk)
+    local checksum = 0
+    for k = #disk, 3, -2 do
+        if disk[k] > 0 then
+            for i = 2, k - 1, 2 do
+                if disk[i] >= disk[k] then
+                    checksum = checksum + (2 * disk_to_index[i] + disk[k] - 1) * disk[k] / 2 * (k - 1) / 2
+                    disk_to_index[i] = disk_to_index[i] + disk[k]
+                    disk[i] = disk[i] - disk[k]
+                    disk[k] = 0
+                end
+            end
+        end
+    end
+
+    for k = 1, #disk, 2 do
+        if disk[k] > 0 then
+            checksum = checksum + (2 * disk_to_index[k] + disk[k] - 1) * disk[k] / 2 * (k - 1) / 2
+        end
+    end
+
+    return checksum
 end
 
 local function main()
@@ -62,11 +114,10 @@ local function main()
 end
 
 -- LuaFormatter off
-test(part1("123"), 6)
 test(part1("12345"), 60)
 test(part1("2333133121414131402"), 1928)
 
-test(part2(""), 0)
+test(part2("2333133121414131402"), 2858)
 -- LuaFormatter on
 
 main()
