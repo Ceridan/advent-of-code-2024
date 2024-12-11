5b09390550c076df63ae7160f8137996484ad295
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2024-12-11 09:54:13 +0100

    feat: Day 11, part 2.

--- src/day11.lua
+++ src/day11.lua
@@ -19,6 +19,7 @@ end
 
 local function count_stones(stones, blinks)
     local cache = {}
+    local first = {}
     print(inspect(stones))
     for i = 1, blinks do
         local new_stones = {}
@@ -32,9 +33,58 @@ local function count_stones(stones, blinks)
             end
         end
         print(i, #stones)
+
+        local f = {}
+        for j = 1, 20 do
+            table.insert(f, new_stones[j])
+        end
+        table.insert(first, f)
+
+        for _, f in ipairs(first) do
+            print(inspect(f))
+        end
+
+        print(i, #stones)
+        print("------------")
         stones = new_stones
     end
     return #stones
+end
+
+local function dfs(stone, blinks, cache)
+    if blinks == 0 then
+        return 1
+    end
+    if cache[stone] ~= nil and cache[stone][blinks] ~= nil then
+        return cache[stone][blinks]
+    end
+
+    local stone1, stone2 = apply_rules(stone)
+    local res = dfs(stone1, blinks-1, cache)
+    if stone2 ~= nil then
+        res = res + dfs(stone2, blinks-1, cache)
+    end
+
+    -- -- rule 1
+    -- if stone == 0 then
+    --     res = dfs(1, blink - 1, cache)
+    -- end
+
+    -- -- rule 2
+    -- local stone_str = tostring(stone)
+    -- local len = string.len(stone_str)
+    -- print(stone, stone_str)
+    -- if len % 2 == 0 then
+    --     local l, r = stone_str:sub(1, len / 2), stone_str:sub(len/2+1, -1)
+    --     res = dfs(tonumber(l), blink-1, cache) + dfs(tonumber(r), blink-1, cache)
+    -- end
+
+    -- -- rule 3
+    -- res = dfs(stone * 2024, blink - 1, cache)
+
+    cache[stone] = cache[stone] or {}
+    cache[stone][blinks] = res
+    return res
 end
 
 local function part1(data, blinks)
@@ -44,21 +94,24 @@ end
 
 local function part2(data, blinks)
     local stones = io.read_matrix(data, " ")[1]
-    -- return count_stones(stones, blinks)
-    return 0
+    local total = 0
+    for _, stone in ipairs(stones) do
+        total = total + dfs(stone, blinks, {})
+    end
+    return total
 end
 
 local function main()
     local input = io.read_file("src/inputs/day11.txt")
 
-    print(string.format("Day 11, part 1: %s", part1(input, 25)))
-    print(string.format("Day 11, part 2: %s", part2(input, 75)))
+    -- print(string.format("Day 11, part 1: %s", part1(input, 25)))
+    print(string.format("Day 11, part 2: %20.0f", part2(input, 75)))
 end
 
 -- LuaFormatter off
-test(part1("0 1 10 99 999", 1), 7)
-test(part1("125 17", 6), 22)
-test(part1("125 17", 25), 55312)
+-- test(part1("0 1 10 99 999", 1), 7)
+-- test(part1("125 17", 6), 22)
+-- test(part1("125 17", 25), 55312)
 -- LuaFormatter on
 
 main()
