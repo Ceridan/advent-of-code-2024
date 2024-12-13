4182967a219120d46b37560a558554e71090966f
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2024-12-13 20:22:40 +0100

    refactor: Cleanup Day 13.

--- src/day13.lua
+++ src/day13.lua
@@ -1,58 +1,25 @@
 local io = require("lib.io")
 local test = require("lib.test")
 local Point2D = require("struct.point2d")
-local inspect = require("inspect")
 
 local A_TOKENS = 3
 local B_TOKENS = 1
-local CLICKS = 100
 
 local function parse_input(input)
     local machines = {}
     local lines = io.read_lines_as_array(input)
-    for i=1, #lines, 3 do
+    for i = 1, #lines, 3 do
         local aiter = lines[i]:gmatch("%d+")
-        local biter = lines[i+1]:gmatch("%d+")
-        local piter = lines[i+2]:gmatch("%d+")
+        local biter = lines[i + 1]:gmatch("%d+")
+        local piter = lines[i + 2]:gmatch("%d+")
         local machine = {
             ["a"] = Point2D.new(tonumber(aiter()), tonumber(aiter())),
             ["b"] = Point2D.new(tonumber(biter()), tonumber(biter())),
-            ["prize"] = Point2D.new(tonumber(piter()), tonumber(piter())),
+            ["prize"] = Point2D.new(tonumber(piter()), tonumber(piter()))
         }
         table.insert(machines, machine)
-     end
-     return machines
-end
-
-local function dfs(machine, pos, a, b, cache)
-    if a > CLICKS or b > CLICKS or pos.x > machine.prize.x or pos.y > machine.prize.y then
-        return 0
     end
-
-    local clicks_key = Point2D.new(a, b):key()
-    if cache[clicks_key] ~= nil and cache[clicks_key][pos:key()] ~= nil then
-        return cache[clicks_key][pos:key()]
-    end
-
-    if a <= CLICKS and b <= CLICKS and pos.x == machine.prize.x and pos.y == machine.prize.y then
-        return a * A_TOKENS + b * B_TOKENS
-    end
-
-    local tokens_a = dfs(machine, pos + machine.a, a + 1, b, cache)
-    local tokens_b = dfs(machine, pos + machine.b, a, b + 1, cache)
-
-    cache[clicks_key] = cache[clicks_key] or {}
-
-    if tokens_a == 0 then
-        cache[clicks_key][pos:key()] = tokens_b
-        return tokens_b
-    elseif tokens_b == 0 then
-        cache[clicks_key][pos:key()] = tokens_a
-        return tokens_a
-    else
-        cache[clicks_key][pos:key()] = math.min(tokens_a, tokens_b)
-        return math.min(tokens_a, tokens_b)
-    end
+    return machines
 end
 
 local function solve(machine)
@@ -60,6 +27,7 @@ local function solve(machine)
     local bx, by = machine.b.x, machine.b.y
     local px, py = machine.prize.x, machine.prize.y
 
+    -- Solution for the system of linear equations with 2 variables.
     local a = math.floor((px * by - bx * py) / (ax * by - bx * ay))
     local b = math.floor((ax * py - px * ay) / (ax * by - bx * ay))
 
@@ -70,34 +38,24 @@ local function solve(machine)
     return 0
 end
 
--- local count_tokens(machines)
-
-local function part1(data)
-    -- local machines = parse_input(data)
-    -- local total_tokens = 0
-    -- for _, machine in ipairs(machines) do
-    --     local cache = {}
-    --     total_tokens = total_tokens + dfs(machine, Point2D.new(0, 0), 0, 0, cache)
-    -- end
-    -- return total_tokens
-
-    local machines = parse_input(data)
+local function count_tokens(machines, correction)
     local total_tokens = 0
     for _, machine in ipairs(machines) do
+        machine.prize.x = machine.prize.x + correction
+        machine.prize.y = machine.prize.y + correction
         total_tokens = total_tokens + solve(machine)
     end
     return total_tokens
 end
 
+local function part1(data)
+    local machines = parse_input(data)
+    return count_tokens(machines, 0)
+end
+
 local function part2(data)
     local machines = parse_input(data)
-    local total_tokens = 0
-    for _, machine in ipairs(machines) do
-        machine.prize.x = machine.prize.x + 10000000000000
-        machine.prize.y = machine.prize.y + 10000000000000
-        total_tokens = total_tokens + solve(machine)
-    end
-    return total_tokens
+    return count_tokens(machines, 10000000000000)
 end
 
 local function main()
