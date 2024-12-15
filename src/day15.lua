753af9cd141cfddeabb00ced98638149706471b7
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2024-12-15 10:43:00 +0100

    feat: Day 15, part 2.

--- src/day15.lua
+++ src/day15.lua
@@ -10,23 +10,6 @@ local DIRECTIONS = {
     ["<"] = Point2D.new(-1, 0),
 }
 
-local function print_map(map, robot, move)
-    print("                       ")
-    print("Move", move)
-    for y = 1, #map do
-        local line = ""
-        for x = 1, #map[y] do
-            if robot.x == x and robot.y == y then
-                line = line .. "@"
-            else
-                line = line .. map[y][x]
-            end
-        end
-        print(line)
-    end
-    print("                       ")
-end
-
 local function parse_input(input)
     local lines = io.read_lines_as_array(input)
     local map = {}
@@ -73,55 +56,20 @@ local function expand_map(map)
     return expanded
 end
 
-local function move_robot(map, robot, dir)
-    local x_end = robot.x
-    local x_step = 1
-    if dir.x > 0 then
-        x_end = #map
-    elseif dir.x < 0 then
-        x_end = 1
-        x_step = -1
-    end
 
-    local y_end = robot.y
-    local y_step = 1
-    if dir.y > 0 then
-        y_end = #map
-    elseif dir.y < 0 then
-        y_end = 1
-        y_step = -1
-    end
-
+local function move_horizontal(map, robot, dir)
     local new_robot = robot + dir
-
-    local box = false
-    for y = new_robot.y, y_end, y_step do
-        for x = new_robot.x, x_end, x_step do
-            if map[y][x] == "." and box == false then
-                return new_robot
-            elseif map[y][x] == "." and box then
-                map[y][x] = map[new_robot.y][new_robot.x]
-                map[new_robot.y][new_robot.x] = "."
-                return new_robot
-            elseif map[y][x] == "#" then
-                return robot
-            else
-                box = true
-            end
-        end
-    end
-    return robot
-end
-
-local function move_right(map, robot)
-    local new_robot = robot + DIRECTIONS[">"]
     local y = new_robot.y
-    for x = new_robot.x, #map[y] do
+    local x_end = #map[1]
+    if dir.x == -1 then
+        x_end = 1
+    end
+    for x = new_robot.x, x_end, dir.x do
         if map[y][x] == "#" then
             return robot
         elseif map[y][x] == "." then
-            for k = x, new_robot.x, -1 do
-                map[y][k] = map[y][k-1]
+            for k = x, new_robot.x, -dir.x do
+                map[y][k] = map[y][k - dir.x]
             end
             return new_robot
         end
@@ -129,15 +77,19 @@ local function move_right(map, robot)
     return robot
 end
 
-local function move_left(map, robot)
-    local new_robot = robot + DIRECTIONS["<"]
-    local y = new_robot.y
-    for x = new_robot.x, 1, -1 do
+local function move_vertical(map, robot, dir)
+    local new_robot = robot + dir
+    local x = new_robot.x
+    local y_end = #map
+    if dir.y == -1 then
+        y_end = 1
+    end
+    for y = new_robot.y, y_end, dir.y do
         if map[y][x] == "#" then
             return robot
         elseif map[y][x] == "." then
-            for k = x, new_robot.x do
-                map[y][k] = map[y][k+1]
+            for k = y, new_robot.y, -dir.y do
+                map[k][x] = map[k - dir.y][x]
             end
             return new_robot
         end
@@ -157,34 +109,6 @@ local function check_wall(map, x, y, dir)
     end
 end
 
--- local function move_box(map, x, y, dir)
---     if map[y][x] == map[y + dir.y][x + dir.x] then
---         move_box(map, x + dir.x, y + dir.y, dir)
---     elseif map[y][x] == "[" and map[y + dir.y][x + dir.x] == "]" then
---         if map[y + dir.y][x + dir.x + 1] == "[" then
---             move_box(map, x + dir.x + 1, y + dir.y, dir)
---         end
---         move_box(map, x + dir.x, y + dir.y, dir)
---     elseif map[y][x] == "]" and map[y + dir.y][x + dir.x] == "[" then
---         if map[y + dir.y][x + dir.x - 1] == "]" then
---             move_box(map, x + dir.x - 1, y + dir.y, dir)
---         end
---         move_box(map, x + dir.x, y + dir.y, dir)
---     end
-
---     if map[y][x] == "[" then
---         map[y + dir.y][x + dir.x] = "["
---         map[y + dir.y][x + dir.x + 1] = "]"
---         map[y][x] = "."
---         map[y][x + 1] = "."
---     else
---         map[y + dir.y][x + dir.x] = "]"
---         map[y + dir.y][x + dir.x - 1] = "["
---         map[y][x] = "."
---         map[y][x - 1] = "."
---     end
--- end
-
 local function move_box(map, x, y, dir)
     if map[y + dir.y][x + dir.x] == "[" then
         move_box(map, x + dir.x, y + dir.y, dir)
@@ -201,7 +125,7 @@ local function move_box(map, x, y, dir)
     map[y][x + 1] = "."
 end
 
-local function move_vertical(map, robot, dir)
+local function move_vertical_wide(map, robot, dir)
     local new_robot = robot + dir
     if check_wall(map, new_robot.x, new_robot.y, dir) then
         return robot
@@ -216,16 +140,19 @@ local function move_vertical(map, robot, dir)
     return new_robot
 end
 
-local function part1(data)
-    local map, moves, robot = parse_input(data)
+local function move_robot(map, moves, robot, vertical_fn, box_symbol)
     for _, move in ipairs(moves) do
-        robot = move_robot(map, robot, DIRECTIONS[move])
+        if move == ">" or move == "<" then
+            robot = move_horizontal(map, robot, DIRECTIONS[move])
+        else
+            robot = vertical_fn(map, robot, DIRECTIONS[move])
+        end
     end
 
     local sum = 0
     for y = 1, #map do
         for x = 1, #map[y] do
-            if map[y][x] == "O" then
+            if map[y][x] == box_symbol then
                 sum = sum + 100 * (y - 1) + (x - 1)
             end
         end
@@ -233,36 +160,16 @@ local function part1(data)
     return sum
 end
 
+local function part1(data)
+    local map, moves, robot = parse_input(data)
+    return move_robot(map, moves, robot, move_vertical, "O")
+end
+
 local function part2(data)
     local map, moves, robot = parse_input(data)
     map = expand_map(map)
     robot = Point2D.new(robot.x * 2 - 1, robot.y)
-
-    print_map(map, robot, "start")
-    print(#moves)
-    for i, move in ipairs(moves) do
-        if move == ">" then
-            robot = move_right(map, robot)
-        elseif move == "<" then
-            robot = move_left(map, robot)
-        else
-            robot = move_vertical(map, robot, DIRECTIONS[move])
-        end
-        print(i)
-        if i > 278 and i < 350 then
-            print_map(map, robot, move)
-        end
-    end
-    print_map(map, robot, "finish")
-    local sum = 0
-    for y = 1, #map do
-        for x = 1, #map[y] do
-            if map[y][x] == "[" then
-                sum = sum + 100 * (y - 1) + (x - 1)
-            end
-        end
-    end
-    return sum
+    return move_robot(map, moves, robot, move_vertical_wide, "[")
 end
 
 local function main()
@@ -349,5 +256,3 @@ v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
 -- LuaFormatter on
 
 main()
-
---309 310
