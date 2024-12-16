dd13422d7f2ca8c4700d55075d85621b95b2110a
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2024-12-16 15:26:45 +0100

    feat: Day 16, part 2.

--- src/day16.lua
+++ src/day16.lua
@@ -2,10 +2,13 @@ local io = require("lib.io")
 local test = require("lib.test")
 local Point2D = require("struct.point2d")
 local Queue = require("struct.queue")
-local inspect = require("inspect")
-local tbl = require("lib.tbl")
 
-local DIRECTIONS = {["E"] = Point2D.new(1, 0), ["S"] = Point2D.new(0, 1), ["W"] = Point2D.new(-1, 0), ["N"] = Point2D.new(0, -1)}
+local DIRECTIONS = {
+    ["E"] = Point2D.new(1, 0),
+    ["S"] = Point2D.new(0, 1),
+    ["W"] = Point2D.new(-1, 0),
+    ["N"] = Point2D.new(0, -1)
+}
 local STEP_SCORES = {
     ["E"] = {["E"] = 1, ["S"] = 1001, ["W"] = 2001, ["N"] = 1001},
     ["S"] = {["E"] = 1001, ["S"] = 1, ["W"] = 1001, ["N"] = 2001},
@@ -13,27 +16,10 @@ local STEP_SCORES = {
     ["N"] = {["E"] = 1001, ["S"] = 2001, ["W"] = 1001, ["N"] = 1}
 }
 
-local function print_map(map, seats)
-    print("                       ")
-    for y = 1, #map do
-        local line = ""
-        for x = 1, #map[y] do
-            local p = Point2D.new(x, y)
-            if seats[p:key()] then
-                line = line .. "O"
-            else
-                line = line .. map[y][x]
-            end
-        end
-        print(line)
-    end
-    print("                       ")
-end
-
 local function parse_input(input)
     local map = io.read_text_matrix(input)
     local start, finish = nil, nil
-    for y =1, #map do
+    for y = 1, #map do
         for x = 1, #map[y] do
             if map[y][x] == "S" then
                 start = Point2D.new(x, y)
@@ -57,12 +43,13 @@ local function bfs(map, start)
             if scores[key] == nil or scores[key][item.dir] == nil or scores[key][item.dir] > item.score then
                 scores[key] = scores[key] or {}
                 scores[key][item.dir] = item.score
+
                 if map[y][x] ~= "E" then
                     for dir, step in pairs(DIRECTIONS) do
                         queue:enqueue({
                             ["pos"] = item.pos + step,
                             ["dir"] = dir,
-                            ["score"] = item.score + STEP_SCORES[item.dir][dir],
+                            ["score"] = item.score + STEP_SCORES[item.dir][dir]
                         })
                     end
                 end
@@ -72,10 +59,7 @@ local function bfs(map, start)
     return scores
 end
 
-local function dfs(map, pos, dir, score, scores, target_score, path, seats)
-    -- if pos == Point2D.new(2, 11) then
-    --     print(score, scores[pos:key()])
-    -- end
+local function dfs(map, scores, target_score, pos, dir, score, path, seats)
     if map[pos.y][pos.x] == "#" or score ~= scores[pos:key()][dir] then
         return
     end
@@ -87,43 +71,39 @@ local function dfs(map, pos, dir, score, scores, target_score, path, seats)
         end
         return
     end
-    -- if pos == Point2D.new(2, 11) then
-    --     print("in")
-    -- end
 
     table.insert(path, pos:key())
     for next_dir, step in pairs(DIRECTIONS) do
         local new_score = score + STEP_SCORES[dir][next_dir]
-        dfs(map, pos + step, next_dir, new_score, scores, target_score, path, seats)
+        dfs(map, scores, target_score, pos + step, next_dir, new_score, path, seats)
     end
     table.remove(path)
 end
 
+local function get_scores(map, start, finish)
+    local scores = bfs(map, start)
+
+    local finish_scores = {}
+    for _, score in pairs(scores[finish:key()]) do
+        table.insert(finish_scores, score)
+    end
+    local best_score = math.min(unpack(finish_scores))
+
+    return best_score, scores
+end
+
 local function part1(data)
     local map, start, finish = parse_input(data)
-    local scores = bfs(map, start)
-    local finish_scores = scores[finish:key()]
-    local values = {}
-    for _, v in pairs(finish_scores) do
-        table.insert(values, v)
-    end
-    return math.min(unpack(values))
+    local score = get_scores(map, start, finish)
+    return score
 end
 
 local function part2(data)
     local map, start, finish = parse_input(data)
-    local scores = bfs(map, start)
-    local values = {}
-    for _, v in pairs(scores[finish:key()]) do
-        table.insert(values, v)
-    end
-    local target_score = math.min(unpack(values))
-
-
+    local target_score, scores = get_scores(map, start, finish)
     local seats = {[start:key()] = true, [finish:key()] = true}
-    dfs(map, start, "E", 0, scores, target_score, {}, seats)
-    print_map(map, seats)
-    -- print(scores[Point2D.new(2, 11):key()])
+    dfs(map, scores, target_score, start, "E", 0, {}, seats)
+
     local count = 0
     for _ in pairs(seats) do
         count = count + 1
@@ -139,7 +119,6 @@ local function main()
 end
 
 -- LuaFormatter off
-print('part1')
 test(part1([[
 ###############
 #.......#....E#
@@ -179,7 +158,7 @@ test(part1([[
 #################
 ]]
 ), 11048)
-print('part2')
+
 test(part2([[
 ###############
 #.......#....E#
@@ -220,7 +199,5 @@ test(part2([[
 ]]
 ), 64)
 -- LuaFormatter on
-
-print('main')
 
 main()
