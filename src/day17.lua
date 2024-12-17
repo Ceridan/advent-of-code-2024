87aed54cfbc6c867afdc450d12d7c883cd39ea75
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2024-12-17 20:57:08 +0100

    feat: Day 17, part 2

--- src/day17.lua
+++ src/day17.lua
@@ -1,7 +1,7 @@
 local io = require("lib.io")
 local test = require("lib.test")
 local bit32 = require("bit32")
-local inspect = require("inspect")
+local Queue = require("struct.queue")
 
 ----------------------------------------
 ---                VM                ---
@@ -16,11 +16,8 @@ function VM.new(a, b, c)
     self._B = b or 0
     self._C = c or 0
     self._output = {}
-    return self
-end
 
-function VM:run(program)
-    local combo = function(val)
+    self._get_combo_operand = function(val)
         if val <= 3 then
             return val
         end
@@ -37,13 +34,13 @@ function VM:run(program)
     end
 
     local adv = function(cmb)
-        self._A = math.floor(self._A / bit32.lshift(2, combo(cmb) - 1))
+        self._A = math.floor(self._A / bit32.lshift(2, self._get_combo_operand(cmb) - 1))
     end
     local bxl = function(lit)
         self._B = bit32.bxor(self._B, lit)
     end
     local bst = function(cmb)
-        self._B = combo(cmb) % 8
+        self._B = self._get_combo_operand(cmb) % 8
     end
     local jnz = function(lit)
         if self._A == 0 then
@@ -55,22 +52,26 @@ function VM:run(program)
         self._B = bit32.bxor(self._B, self._C)
     end
     local out = function(cmb)
-        table.insert(self._output, combo(cmb) % 8)
+        table.insert(self._output, self._get_combo_operand(cmb) % 8)
     end
     local bdv = function(cmb)
-        self._B = math.floor(self._A / bit32.lshift(2, combo(cmb) - 1))
+        self._B = math.floor(self._A / bit32.lshift(2, self._get_combo_operand(cmb) - 1))
     end
     local cdv = function(cmb)
-        self._C = math.floor(self._A / bit32.lshift(2, combo(cmb) - 1))
+        self._C = math.floor(self._A / bit32.lshift(2, self._get_combo_operand(cmb) - 1))
     end
 
-    local opcodes = { adv, bxl, bst, jnz, bxc, out, bdv, cdv, }
+    self._opcodes = {adv, bxl, bst, jnz, bxc, out, bdv, cdv}
 
+    return self
+end
+
+function VM:run(program)
     local p = 0
     while p < #program do
         local opcode = program[p + 1]
         local operand = program[p + 2]
-        local res = opcodes[opcode + 1](operand) or (-1)
+        local res = self._opcodes[opcode + 1](operand) or (-1)
         if res >= 0 then
             p = res
         else
@@ -78,11 +79,14 @@ function VM:run(program)
         end
     end
 
-    return table.concat(self._output, ",")
+    return self._output
 end
 
-function VM:print_program()
-    return table.concat(self._program, ",")
+function VM:reset(a, b, c)
+    self._A = a or 0
+    self._B = b or 0
+    self._C = c or 0
+    self._output = {}
 end
 
 function VM:__tostring()
@@ -93,7 +97,7 @@ Register C: %d
 
 Program: %s
 Output: %s
-]], self._A, self._B, self._C, table.concat(self._program, ","),  table.concat(self._output, ","))
+]], self._A, self._B, self._C, table.concat(self._program, ","), table.concat(self._output, ","))
 end
 
 ----------------------------------------
@@ -112,21 +116,52 @@ local function parse_input(input)
     return a, b, c, program
 end
 
+local function validate(program, output, pos)
+    for i = 1, #output do
+        if program[#program - pos + i] ~= output[i] then
+            return false
+        end
+    end
+    return true
+end
+
+local function reverse_engineering(vm, program)
+    local queue = Queue.new()
+    queue:enqueue({["a"] = 0, ["pos"] = 1})
+    while not queue:is_empty() do
+        local item = queue:dequeue()
+        for a = item.a, item.a + 7 do
+            vm:reset(a)
+            local output = vm:run(program)
+            if validate(program, output, item.pos) then
+                if item.pos == #program then
+                    return a
+                end
+                queue:enqueue({["a"] = a * 8, ["pos"] = item.pos + 1})
+            end
+        end
+    end
+    return 0
+end
+
 local function part1(data)
     local a, b, c, program = parse_input(data)
     local vm = VM.new(a, b, c)
-    return vm:run(program)
+    local output = vm:run(program)
+    return table.concat(output, ",")
 end
 
 local function part2(data)
-    return 0
+    local a, b, c, program = parse_input(data)
+    local vm = VM.new(a, b, c)
+    return reverse_engineering(vm, program)
 end
 
 local function main()
     local input = io.read_file("src/inputs/day17.txt")
 
     print(string.format("Day 17, part 1: %s", part1(input)))
-    print(string.format("Day 17, part 2: %s", part2(input)))
+    print(string.format("Day 17, part 2: %10.0f", part2(input)))
 end
 
 -- LuaFormatter off
@@ -154,13 +189,13 @@ Register C: 0
 Program: 0,1,5,4,3,0
 ]]), "4,6,3,5,6,3,5,2,1,0")
 
--- test(part2([[
--- Register A: 2024
--- Register B: 0
--- Register C: 0
+test(part2([[
+Register A: 2024
+Register B: 0
+Register C: 0
 
--- Program: 0,3,5,4,3,0
--- ]]), 117440)
+Program: 0,3,5,4,3,0
+]]), 117440)
 -- LuaFormatter on
 
 main()
