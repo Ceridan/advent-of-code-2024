2393d89904384322d7112655d3c2d4f170916c8f
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2024-12-19 21:55:17 +0100

    feat: Day 19, part 2.

--- src/day19.lua
+++ src/day19.lua
@@ -1,6 +1,5 @@
 local io = require("lib.io")
 local test = require("lib.test")
-local inspect = require("inspect")
 
 local function parse_input(input)
     local lines = io.read_lines_as_array(input)
@@ -25,37 +24,50 @@ local function dfs(towels, word, i, cache)
         return cache[i]
     end
 
+    local res = 0
     for _, towel in ipairs(towels) do
         if towel == word:sub(i, i + string.len(towel) - 1) then
-            cache[i] = dfs(towels, word, i + string.len(towel), cache)
-            if cache[i] == 1 then
-                return 1
-            end
+            res = res + dfs(towels, word, i + string.len(towel), cache)
         end
     end
 
-    cache[i] = 0
-    return 0
+    cache[i] = res
+    return res
 end
 
-local function part1(data)
-    local towels, patterns = parse_input(data)
+local function calculate_patterns(towels, patterns, reduce_fn)
     local possible = 0
     for _, word in ipairs(patterns) do
-        possible = possible + dfs(towels, word, 1, {})
+        local res = dfs(towels, word, 1, {})
+        possible = reduce_fn(possible, res)
     end
     return possible
 end
 
+local function part1(data)
+    local towels, patterns = parse_input(data)
+    local reducer = function(acc, num)
+        if num > 0 then
+            return acc + 1
+        end
+        return acc
+    end
+    return calculate_patterns(towels, patterns, reducer)
+end
+
 local function part2(data)
-    return 0
+    local towels, patterns = parse_input(data)
+    local reducer = function(acc, num)
+        return acc + num
+    end
+    return calculate_patterns(towels, patterns, reducer)
 end
 
 local function main()
     local input = io.read_file("src/inputs/day19.txt")
 
     print(string.format("Day 19, part 1: %s", part1(input)))
-    print(string.format("Day 19, part 2: %s", part2(input)))
+    print(string.format("Day 19, part 2: %10.0f", part2(input)))
 end
 
 -- LuaFormatter off
@@ -74,7 +86,7 @@ bbrgwb
 
 test(part1(TEST_INPUT), 6)
 
-test(part2(TEST_INPUT), 0)
+test(part2(TEST_INPUT), 16)
 -- LuaFormatter on
 
 main()
