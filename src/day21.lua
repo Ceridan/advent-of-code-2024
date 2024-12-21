6fbb052c24995e568691309cf459ba286ad81f3f
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2024-12-21 17:37:09 +0100

    feat: Day 21, part 2.

--- src/day21.lua
+++ src/day21.lua
@@ -17,11 +17,13 @@ function PointWithKey:key()
     return self.key
 end
 
+-- local DIRECTIONS = { ">", "v", "^", "<" }
+-- local DIRECTIONS = { "<", "^", "v", ">" }
 local DIRECTIONS = {
     [">"] = Point2D.new(1, 0),
     ["v"] = Point2D.new(0, 1),
-    ["<"] = Point2D.new(-1, 0),
     ["^"] = Point2D.new(0, -1),
+    ["<"] = Point2D.new(-1, 0),
 }
 
 -- +---+---+---+
@@ -130,6 +132,98 @@ local DIR_PAD = {
 --     ["A"] = {["^"] = 1, [">"] = 1, ["v"] = 2, ["<"] = 3, ["A"] = 0},
 -- }
 
+
+local function list_shortest_paths(pad, from, to)
+    local paths = {}
+    local from_pos = pad.btn_to_pos[from]
+    local to_pos = pad.btn_to_pos[to]
+    local queue = Queue.new()
+    queue:enqueue({["pos"] = from_pos, ["path"] = ""})
+    while not queue:is_empty() do
+        local item = queue:dequeue()
+        if item.pos == to_pos then
+            table.insert(paths, item.path .. "A")
+        else
+            local dist = item.pos:manhattan(to_pos)
+            for k, dir in pairs(DIRECTIONS) do
+                local pos = item.pos + dir
+                if pad.pos_to_btn[pos:key()] and pos:manhattan(to_pos) < dist then
+                    queue:enqueue({["pos"] = pos, ["path"] = item.path .. k})
+                end
+            end
+        end
+    end
+    return paths
+end
+
+local function build_path_map(pad)
+    local path_map = {}
+    for key1, pos1 in pairs(pad.btn_to_pos) do
+        for key2, pos2 in pairs(pad.btn_to_pos) do
+            local paths = list_shortest_paths(pad, key1, key2)
+            path_map[key1] = path_map[key1] or {}
+            path_map[key1][key2] = {["paths"] = paths, ["len"] = pos1:manhattan(pos2) + 1}
+        end
+    end
+    return path_map
+end
+
+local function dpp(seq, pads, dir_pads_map, cache)
+    if cache[pads] and cache[pads][seq] then
+        return cache[pads][seq]
+    end
+
+    local len = 0
+    local k1 = "A"
+    for i = 1, string.len(seq) do
+        local k2 = seq:sub(i, i)
+        if pads == 0 then
+            len = len + dir_pads_map[k1][k2].len
+        else
+            local min_len = 0
+            for _, path in ipairs(dir_pads_map[k1][k2].paths) do
+                local path_len = dpp(path, pads - 1, dir_pads_map, cache)
+                if min_len == 0 or min_len > path_len then
+                    min_len = path_len
+                end
+            end
+            len = len + min_len
+        end
+        k1 = k2
+    end
+    cache[pads] = cache[pads] or {}
+    cache[pads][seq] = len
+    return len
+end
+
+local function calculate_code_complexity_dpp(code, pads)
+    local num_map = build_path_map(NUM_PAD)
+    local dir_map = build_path_map(DIR_PAD)
+
+    local cache = {}
+    local len = 0
+    local k1 = "A"
+    for i = 1, string.len(code) do
+        local k2 = code:sub(i, i)
+        local min_len = 0
+        for _, path in ipairs(num_map[k1][k2].paths) do
+            local path_len = dpp(path, pads - 1, dir_map, cache)
+            if min_len == 0 or min_len > path_len then
+                min_len = path_len
+            end
+            print(k1, k2, path, path_len, len)
+        end
+        len = len + min_len
+        k1 = k2
+    end
+
+    print(inspect(cache))
+    print(len)
+    print(inspect(dir_map["<"]["A"]))
+    return len * tonumber(code:sub(1, -2))
+end
+
+
 -- local function num_pad_bfs(code)
 --     local code_arr = {}
 --     code:gsub(".", function(ch) table.insert(code_arr, ch) end)
@@ -227,6 +321,7 @@ local DIR_PAD = {
 -- end
 
 
+
 local function dfs(pad, code, idx, curr, seq, sequences)
     if idx == #code + 1 then
         table.insert(sequences, table.concat(seq))
@@ -252,6 +347,42 @@ local function dfs(pad, code, idx, curr, seq, sequences)
             table.remove(seq)
         end
     end
+end
+
+
+-- local function dp(pad, sequence, depth, dir_pads, cache)
+--     if depth == dir_pads then
+--         return string.len(sequence)
+--     end
+
+--     if cache[sequence] and cache[sequence][depth] then
+--         return cache[sequence][depth]
+--     end
+
+
+-- end
+
+local function dfs2(pad, code, idx, curr, seq)
+    if idx == #code + 1 then
+        return seq
+    end
+
+    local target = code:sub(idx, idx)
+    if target == curr then
+        return dfs2(pad, code, idx + 1, curr, seq .. "A")
+    end
+
+    local curr_pos = pad.btn_to_pos[curr]
+    local target_pos = pad.btn_to_pos[target]
+    local dist = curr_pos:manhattan(target_pos)
+    for _, k in ipairs(DIRECTIONS) do
+        local dir = DIR_TO_COORD[k]
+        local next_pos = curr_pos + dir
+        if pad.pos_to_btn[next_pos:key()] and next_pos:manhattan(target_pos) < dist then
+            return dfs2(pad, code, idx, pad.pos_to_btn[next_pos:key()], seq .. k)
+        end
+    end
+    return ""
 end
 
 local function select_sequence(sequences)
@@ -381,6 +512,110 @@ end
 --     return complexity
 -- end
 
+local function calculate_length(code, start, dir_pads)
+    local seqs = {}
+    dfs(NUM_PAD, code, 1, start, {}, seqs)
+    for i = 1, dir_pads do
+        print(i)
+        local all_seqs = {}
+        for _, s in ipairs(select_sequence(seqs)) do
+            dfs(DIR_PAD, s, 1, "A", {}, all_seqs)
+        end
+        seqs = all_seqs
+    end
+
+    local len = 0
+    for _, s in ipairs(seqs) do
+        if len == 0 or string.len(s) < len then
+            len = string.len(s)
+        end
+    end
+    return len
+end
+
+
+local function calculate_length_2(code, start, dir_pads)
+    local seq = dfs2(NUM_PAD, code, 1, start, "")
+    for i = 1, dir_pads do
+        print(seq)
+        seq = dfs2(DIR_PAD, seq, 1, "A", "")
+    end
+    return string.len(seq)
+end
+
+
+local function build_dir_pad_transitions()
+    local transitions = {}
+    for key1 in pairs(DIR_PAD.btn_to_pos) do
+        for key2 in pairs(DIR_PAD.btn_to_pos) do
+            local seqs = {}
+            dfs(DIR_PAD, key2, 1, key1, {}, seqs)
+            local len = 0
+            for _, s in ipairs(seqs) do
+                if len == 0 or string.len(s) < len then
+                    len = string.len(s)
+                end
+            end
+            transitions[key1] = transitions[key1] or {}
+            transitions[key1][key2] = len
+        end
+    end
+    return transitions
+end
+
+local function build_num_pad_transitions()
+    local transitions = {}
+    for key1 in pairs(NUM_PAD.btn_to_pos) do
+        for key2 in pairs(NUM_PAD.btn_to_pos) do
+            local seqs = {}
+            dfs(NUM_PAD, key2, 1, key1, {}, seqs)
+            local len = 0
+            for _, s in ipairs(seqs) do
+                if len == 0 or string.len(s) < len then
+                    len = string.len(s)
+                end
+            end
+            transitions[key1] = transitions[key1] or {}
+            transitions[key1][key2] = len
+        end
+    end
+    return transitions
+end
+
+-- local function calculate_code(code, dir_pads)
+--     local dir_transitions = build_dir_pad_transitions()
+--     local num_transitions = build_num_pad_transitions()
+--     local total_len = 0
+--     local ch = "A"
+--     for i = 1, #code do
+--         local len = num_transitions[ch][code:sub(i, i)]
+--         for j = 1, dir_pads do
+--             len = len * dir_transitions["A"][]
+--         end
+
+--         ch = code:sub(i, i)
+--     end
+
+--     return total_len * tonumber(code:sub(1, -2))
+-- end
+
+local function build_transitions(dir_pads)
+    local transitions = {}
+    for key1 in pairs(NUM_PAD.btn_to_pos) do
+        for key2 in pairs(NUM_PAD.btn_to_pos) do
+            -- if key1 <= key2 then
+                local len = calculate_length(key2, key1, dir_pads)
+                transitions[key1] = transitions[key1] or {}
+                transitions[key1][key2] = len
+                print(key1, key2, len)
+                -- transitions[key2] = transitions[key2] or {}
+                -- transitions[key2][key1] = len
+            -- end
+        end
+    end
+    return transitions
+end
+
 local function process_code(code, dir_pads)
     local seqs = {}
     dfs(NUM_PAD, code, 1, "A", {}, seqs)
@@ -411,52 +646,74 @@ local function calculate_complexity(codes, dir_pads)
     return complexity
 end
 
+-- local function part1(data, dir_pads)
+--     local codes = io.read_lines_as_array(data)
+--     return calculate_complexity(codes, dir_pads)
+-- end
+
+-- local function dfs3(code, )
+-- end
+
 local function part1(data, dir_pads)
     local codes = io.read_lines_as_array(data)
-    return calculate_complexity(codes, dir_pads)
+    local complexity = 0
+    for _, code in ipairs(codes) do
+        complexity = complexity + calculate_code_complexity_dpp(code, dir_pads)
+    end
+    return complexity
+    -- local transitions = build_transitions(dir_pads)
     -- local complexity = 0
     -- for _, code in ipairs(codes) do
-    --     local len = 0
-    --     local seq1 = {}
-    --     dfs(NUM_PAD, code, 1, "A", {}, seq1)
-    --     for _, s1 in ipairs(select_sequence(seq1)) do
-    --         local seq2 = {}
-    --         dfs(DIR_PAD, s1, 1, "A", {}, seq2)
-    --         for _, s2 in ipairs(select_sequence(seq2)) do
-    --             local seq3 = {}
-    --             dfs(DIR_PAD, s2, 1, "A", {}, seq3)
-    --             for _, s3 in ipairs(seq3) do
-    --                 if len == 0 or string.len(s3) < len then
-    --                     len = string.len(s3)
-    --                 end
-    --             end
-    --         end
+    --     local code_len = 0
+    --     local ch = "A"
+    --     for i = 1, string.len(code) do
+    --         code_len = code_len + transitions[ch][code:sub(i, i)]
+    --         ch = code:sub(i, i)
     --     end
-    --     print(code, len)
-    --     complexity = complexity + len * tonumber(code:sub(1,-2))
+    --     complexity = complexity + code_len * tonumber(code:sub(1,-2))
     -- end
     -- return complexity
+
 end
 
 local function part2(data, dir_pads)
-    return 0
+    local codes = io.read_lines_as_array(data)
+    local complexity = 0
+    for _, code in ipairs(codes) do
+        complexity = complexity + calculate_code_complexity_dpp(code, dir_pads)
+    end
+    return complexity
 end
+
+
+-- local function part2(data, dir_pads)
+--     local codes = io.read_lines_as_array(data)
+--     local transitions = build_transitions(dir_pads)
+--     local complexity = 0
+--     for _, code in ipairs(codes) do
+--         local code_len = 0
+--         local ch = "A"
+--         for i = 1, string.len(code) do
+--             code_len = code_len + transitions[ch][code:sub(i, i)]
+--             ch = code:sub(i, i)
+--         end
+--         complexity = complexity + code_len * tonumber(code:sub(1,-2))
+--     end
+--     return complexity
+-- end
 
 local function main()
     local input = io.read_file("src/inputs/day21.txt")
 
     print(string.format("Day 21, part 1: %s", part1(input, 2)))
-    print(string.format("Day 21, part 2: %s", part2(input, 25)))
+    print(string.format("Day 21, part 2: %20.0f", part2(input, 25)))
 end
 
 -- LuaFormatter off
-print("test1 - 029A")
 test(part1("029A", 2), 1972)
 
-print("test1 - 379A")
 test(part1("379A", 2), 24256)
 
-print("test2")
 test(part1([[
 029A
 980A
@@ -466,7 +723,6 @@ test(part1([[
 ]], 2), 126384)
 -- LuaFormatter on
 
-print("main")
 main()
 
 
---
+++ src/tmp.lua
@@ -0,0 +1,2 @@
+local a = {1,2,3,4,5}
+print(math.min(unpack(a)))
